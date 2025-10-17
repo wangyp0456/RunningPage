f3d5c8fa19a2d05ae3641d5f73e11fc38b918223
wangyp0456 <351241511@qq.com>
2025-10-17 15:51:42 +0800

    v1.0

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Wangyp.Running',
+  siteUrl: '',
+  logo: '/src/static/logo.jpg',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Blog',
+      url: '',
+    },
+    {
+      name: 'Thinks',
+      url: 'https://github.com/yihong0618/running_page',
+    },
+  ],
+};
+
+export default data;
